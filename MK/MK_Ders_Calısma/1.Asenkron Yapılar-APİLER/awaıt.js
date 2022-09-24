1686047e41d0494c126b85956bb376697290676d
Mustafa_Kayıkcı <mktr6363@gmail.com>
2022-09-24 12:11:33 +0200

    yenı klasor eklendı

---
+++ MK/MK_Ders_Calısma/1.Asenkron Yapılar-APİLER/awaıt.js
@@ -0,0 +1,7 @@
+/**daha cok degıskenlere atama yaparak yazmak daha uygundur.
+ * API keyımızı de degısken halıne getırıyoruz. hep kullanacagımız ıcın en guzelı bı rdegıskene atamak.
+ *
+ * try catch yapısı, hata yonetım sıstemıdır. try code blogu ıcınde yer alanları calıstır eger sorun cıkarsa bunu yonetetıyoruz.
+ * desctruptıon yontemını cok kullan 43.satır
+ *
+ */
